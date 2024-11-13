5b886b1d8207ff07843779b7012f3b23a15641f3
Artem Shakirov <art.sha03@gmail.com>
2024-11-13 11:23:29 +0100

    Added input from txt file, updated CLI

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 use anyhow::Result;
 use poe_gem_parser::*;
 use std::env;
+use std::fs;
 
 fn help() {
     println!("PoE Gem Parser -- Help");
-    println!("cargo run <GemText>        Parse gem text");
-    println!("cargo run -- --help        Help");
-    println!("cargo run -- --credits     Credits");
+    println!("cargo run <GemText>                               Parse gem text");
+    println!("cargo run -- --file <iunput_file> <output_file>   Parse gem from txt file");
+    println!("cargo run -- --help                               Help");
+    println!("cargo run -- --credits                            Credits");
 }
 
 fn credits() {
@@ -27,6 +29,17 @@ fn main() -> Result<()> {
     match args[1].as_str() {
         "--help" => help(),
         "--credits" => credits(),
+        "--file" => {
+            if args.len() < 4 {
+                println!("Wrong number of arguments for parsing file");
+                return Ok(());
+            } 
+            let input_file = args[2].as_str();
+            let output_file = args[3].as_str();
+            let input = fs::read_to_string(input_file)?;
+            let parsed = ParsedGem::parse(input.as_str())?;
+            fs::write(output_file, format!("{:#?}", parsed))?;
+        },
         gem => println!("{:#?}", ParsedGem::parse(gem)?)
     }
 
